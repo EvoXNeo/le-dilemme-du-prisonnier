8cf39c2da777dd273eaf4e23635dc568ee69532f
EvoXNeo <evox.neo@gmail.com>
2020-02-02 04:28:04 +0100

    start

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-# -*- coding: utf-8 -*-
+
 """
-Created on Thu Dec 12 17:15:03 2019
-
-@author: lucas cosson
+Le dilemme du prisonnier
+@author: fabian devel - medhi louison - lucas cosson
 """
 
+"""
+ALL FUNCTION
+"""
+def copycat() :
+    return
 
+def all_cooperate():
+    return
+
+def all_cheat():
+    return
+
+def grudger():
+    return
+
+def detective():
+    return
+
+"""
+MAIN
+"""
